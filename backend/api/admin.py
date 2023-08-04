c31f4aa6eb399ed8e76a9544b52be7a7bb4d585a
utchchhwas <usingha00@gmail.com>
2023-08-04 18:09:12 +0600

    Added custom user with email as identifier

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -2,6 +2,46 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import MyUser
 
-# Register your models here.
 
-admin.site.register(MyUser, UserAdmin)
+class MyUserAdmin(UserAdmin):
+    """
+    Custom User Admin.
+    """
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("first_name", "last_name", "email")
+    ordering = ("email",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
+
+
+admin.site.register(MyUser, MyUserAdmin)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,61 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractUser,
+)
+from django.db import models
 
-# Create your models here.
+
+class MyUserManager(BaseUserManager):
+    """
+    Custom User Manager.
+    """
+
+    def create_user(self, email, username, password):
+        """
+        Create a new user with email as primary identifier.
+        """
+
+        if not email:
+            raise ValueError("User must have an email.")
+
+        if not password:
+            raise ValueError("User must have a password.")
+
+        user = self.model(email=self.normalize_email(email), username=username)
+
+        user.set_password(password)
+
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, username, password):
+        """
+        Create a new user with superuser privileges.
+        """
+
+        user = self.create_user(email, username, password)
+
+        user.is_staff = True  # Make accessible from Django Admin
+        user.is_superuser = True  # Grant all permission in Django Admin
+
+        user.save(using=self._db)
+
+        return user
+
 
 class MyUser(AbstractUser):
-    pass
+    """
+    Custom User Model.
+    """
+
+    email = models.EmailField("email address", unique=True)
+    username = None  # Remove username field
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
