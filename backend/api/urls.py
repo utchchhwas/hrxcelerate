0fd0ebfe00948c4ad6644e062d8f55e8f64b0831
utchchhwas <usingha00@gmail.com>
2023-08-05 18:01:14 +0600

    Added token urls

---
+++ backend/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+urlpatterns = [
+    path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
