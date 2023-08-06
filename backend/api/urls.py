cb52c3ea26fc2a8d8e2a8092bf017a75e127991f
Shahriar-Ferdoush <1805101@ugrad.cse.buet.ac.bd>
2023-08-07 00:14:27 +0600

    Updated Model Viewsets in urls, and inits

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,9 +5,23 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from api.views import RetrieveUpdateCompanyView
-from api.views import EmployeeViewSet
+
+# Importing viewsets for All models
+from api.views import ApplicantViewSet
+from api.views import CompanyViewSet
+from api.views import CustomUserViewSet
 from api.views import DepartmentViewSet
+from api.views import EmployeeViewSet
+from api.views import EmploymentViewSet
+from api.views import InterviewerViewSet
+from api.views import InterviewResultViewSet
+from api.views import JobPostingViewSet
+from api.views import JobPostingSalaryViewSet
 from api.views import JobRoleViewSet
+from api.views import NotificationViewSet
+from api.views import PayslipViewSet
+from api.views import TimeOffViewSet
+from api.views import TrackingViewSet
 
 urlpatterns = [
     path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
@@ -16,9 +30,22 @@ urlpatterns = [
 ]
 
 
+# Registering viewsets for All models
 router = DefaultRouter()
-router.register(r"employee", EmployeeViewSet, basename="employee")
+router.register(r"applicant", ApplicantViewSet, basename="applicant")
+router.register(r"company", CompanyViewSet, basename="company")
+router.register(r"customuser", CustomUserViewSet, basename="customuser")
 router.register(r"department", DepartmentViewSet, basename="department")
-router.register(r"job-role", JobRoleViewSet, basename="job-role")
+router.register(r"employee", EmployeeViewSet, basename="employee")
+router.register(r"employment", EmploymentViewSet, basename="employment")
+router.register(r"interviewer", InterviewerViewSet, basename="interviewer")
+router.register(r"interviewresult", InterviewResultViewSet, basename="interviewresult")
+router.register(r"jobposting", JobPostingViewSet, basename="jobposting")
+router.register(r"jobpostingsalary", JobPostingSalaryViewSet, basename="jobpostingsalary")
+router.register(r"jobrole", JobRoleViewSet, basename="jobrole")
+router.register(r"notification", NotificationViewSet, basename="notification")
+router.register(r"payslip", PayslipViewSet, basename="payslip")
+router.register(r"timeoff", TimeOffViewSet, basename="timeoff")
+router.register(r"tracking", TrackingViewSet, basename="tracking")
 
 urlpatterns += router.urls
--- backend/api/views/__init__.py
+++ backend/api/views/__init__.py
@@ -1,4 +1,20 @@
-from .RetrieveUpdateCompanyView import RetrieveUpdateCompanyView
-from .EmployeeViewSet import EmployeeViewSet
+# View Sets for Allicant, Company, CustomUser, Department, Employee, Employment,
+# Interviewer, InterviewResult, JobPosting, JobPostingSalary, JobRole, Notification,
+# Payslip, TimeOff and Tracking models.
+
+from .ApplicantViewSet import ApplicantViewSet
+from .CompanyViewSet import CompanyViewSet
+from .CustomUserViewSet import CustomUserViewSet
 from .DepartmentViewSet import DepartmentViewSet
+from .EmployeeViewSet import EmployeeViewSet
+from .EmploymentViewSet import EmploymentViewSet
+from .InterviewerViewSet import InterviewerViewSet
+from .InterviewResultViewSet import InterviewResultViewSet
+from .JobPostingViewSet import JobPostingViewSet
+from .JobPostingSalaryViewSet import JobPostingSalaryViewSet
 from .JobRoleViewSet import JobRoleViewSet
+from .NotificationViewSet import NotificationViewSet
+from .PayslipViewSet import PayslipViewSet
+from .TimeOffViewSet import TimeOffViewSet
+from .TrackingViewSet import TrackingViewSet
+
