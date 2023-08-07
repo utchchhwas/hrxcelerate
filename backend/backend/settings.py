333eb11e866baaa5573ea2b67b6a9b25d4da201a
utchchhwas <usingha00@gmail.com>
2023-08-07 13:56:12 +0600

    Updaded Docker configs

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from dotenv import load_dotenv
-import os
 
 
 # Load environment variables
@@ -27,12 +27,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-r**z97k$^63(--v=%zxxg!hm_@g(mrk*v3178ofh3-g1_&m$g-"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=False))
 
-ALLOWED_HOSTS = []
+# 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
+# For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -106,23 +108,23 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("RENDER_POSTGRESQL_NAME"),
-        "USER": os.getenv("RENDER_POSTGRESQL_USER"),
-        "PASSWORD": os.getenv("RENDER_POSTGRESQL_PASS"),
-        "HOST": os.getenv("RENDER_POSTGRESQL_HOST"),
-        "PORT": os.getenv("RENDER_POSTGRESQL_PORT"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": os.getenv("RENDER_POSTGRESQL_NAME"),
+#         "USER": os.getenv("RENDER_POSTGRESQL_USER"),
+#         "PASSWORD": os.getenv("RENDER_POSTGRESQL_PASS"),
+#         "HOST": os.getenv("RENDER_POSTGRESQL_HOST"),
+#         "PORT": os.getenv("RENDER_POSTGRESQL_PORT"),
+#     }
+# }
 
 
 # Password validation
@@ -159,7 +161,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
