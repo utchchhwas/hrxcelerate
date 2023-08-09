51f48a65ed4ff0836f3d7e9624db8d9978a01395
utchchhwas <usingha00@gmail.com>
2023-08-09 07:06:56 +0600

    Modified endpoints name

--- backend/entrypoint.dev.sh
+++ backend/entrypoint.dev.sh
@@ -10,7 +10,6 @@ if [[ "$DATABASE" = "postgres" ]]; then
     echo "PostgreSQL started."
 fi
 
-# python manage.py flush --no-input
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
 
