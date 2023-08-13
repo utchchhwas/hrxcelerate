0d15d5938d768095b915bed4bb38156ed73a946a
Shahriar-Ferdoush <1805101@ugrad.cse.buet.ac.bd>
2023-08-14 01:44:59 +0600

    Added Basic Info and People

---
+++ frontend/src/Components/BasicInfo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import Navbar from "./Navbar";
+
+function BasicInfo() {
+  return (
+    <div>
+        <Navbar />
+        <h1>Basic Informations</h1>
+    </div>
+  );
+}
---
+++ frontend/src/Components/People.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import Navbar from "./Navbar";
+
+function People() {
+  return (
+    <div>
+        <Navbar />
+        <h1>People</h1>
+    </div>
+  );
+}
