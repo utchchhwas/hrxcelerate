ff4a4ca7c4623e2be9f6c82b785ee4e984bc7bd3
utchchhwas <usingha00@gmail.com>
2023-08-14 02:52:43 +0600

    Added comment

--- backend/api/views/company_views.py
+++ backend/api/views/company_views.py
@@ -1,20 +1,21 @@
 from rest_framework import viewsets, mixins
 from api.models import Company
 from api.serializers import CompanySerializer
-from rest_framework.permissions import (
-    AllowAny,
-    IsAuthenticated,
-    SAFE_METHODS,
-)
+from rest_framework.permissions import AllowAny, IsAuthenticated, SAFE_METHODS
 from api.permissions import IsAdminEmployee, IsCompanyObject
 
 
 class CompanyRetrieveUpdateViewSet(
     mixins.RetrieveModelMixin,
     mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
     viewsets.GenericViewSet,
 ):
+    """
+    ViewSet for Company supporting the following actions:
+        - Retrieve details
+        - Update
+    """
+
     serializer_class = CompanySerializer
     queryset = Company.objects.all()
 
