2dac1b8ce9e60c0c20cfa2072c379b19a9a4a7f3
Shahriar-Ferdoush <1805101@ugrad.cse.buet.ac.bd>
2023-08-14 06:21:29 +0600

    updating packages and fixing start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from './Components/Login';
 import Departments from './Components/Departments';
 
 const App = () => {
-  // return <Login />;
-  return <Departments />;
+  return (
+    <Departments />
+  );
 };
 
 export default App;
