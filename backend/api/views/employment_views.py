a076f75460431ce39d5cc8693b78c751425e1501
utchchhwas <usingha00@gmail.com>
2023-08-18 05:04:50 +0600

    Tested Employment

--- backend/api/views/employment_views.py
+++ backend/api/views/employment_views.py
@@ -2,13 +2,35 @@ from rest_framework import viewsets
 from api.models import Employment
 from api.serializers import EmploymentSerializer
 from api.permissions import CompanyPermissionMixin
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class EmploymentViewSet(
     CompanyPermissionMixin,
     viewsets.ModelViewSet,
 ):
-    """ """
+    """
+    Provides the following actions:
+        - Create Employment
+        - Retrieve Employment List
+        - Retrieve Employment
+        - Update Employment
+        - Destroy Employment
+    """
+
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = [
+        "id",
+        "employee",
+        "job_role",
+        "is_active",
+        "employment_type",
+        "is_remote",
+    ]
+    search_fields = ["employee__user__email", "job_role__name"]
+    ordering_fields = ["id", "employee__user__email", "job_role__name"]
+    ordering = ["id"]
 
     serializer_class = EmploymentSerializer
 
