b63e352709f670ffac7fb26c68719e20393f51db
utchchhwas <usingha00@gmail.com>
2023-08-18 17:49:07 +0600

    Tested Job Posting endpoints

--- backend/api/serializers/job_posting_serializers.py
+++ backend/api/serializers/job_posting_serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from api.models import JobPosting
+from api.serializers import JobRoleSerializer
 from rest_flex_fields.serializers import FlexFieldsSerializerMixin
 
 
@@ -11,14 +12,24 @@ class JobPostingSerializer(
     Serializer for JobPosting model.
     """
 
-    job_role_name = serializers.CharField(source="job_role.name", read_only=True)
-
     class Meta:
         model = JobPosting
         fields = [
+            "id",
             "job_role",
-            "job_role_name",
             "tags",
             "description",
             "is_active",
         ]
+        expandable_fields = {
+            "job_role": (
+                JobRoleSerializer,
+                {"expand": ["department"]},
+            )
+        }
+
+    def validate_job_role(self, value):
+        employee = self.context["request"].user.employee
+        if value.department.company != employee.company:
+            raise serializers.ValidationError("Invalid Job Role.")
+        return value
--- backend/api/views/job_posting_views.py
+++ backend/api/views/job_posting_views.py
@@ -1,10 +1,48 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 from api.models import JobPosting
 from api.serializers import JobPostingSerializer
+from api.permissions import CompanyPermissionMixin
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 
-class JobPostingViewSet(ModelViewSet):
-    """ """
+class JobPostingViewSet(
+    CompanyPermissionMixin,
+    viewsets.ModelViewSet,
+):
+    """
+    Provides the following actions:
+        - Create Job Posting
+        - Retrieve Job Posting List
+        - Retrieve Job Posting
+        - Update Job Posting
+        - Destroy Job Posting
+    """
 
-    queryset = JobPosting.objects.all()
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = [
+        "id",
+        "job_role",
+        "job_role__department",
+        "is_active",
+    ]
+    search_fields = [
+        "job_role__name",
+        "tags",
+    ]
+    ordering_fields = [
+        "job_role__name",
+        "job_role",
+    ]
+    ordering = [
+        "job_role__name",
+    ]
+
     serializer_class = JobPostingSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if not hasattr(user, "employee"):
+            return JobPosting.objects.none()
+        company = user.employee.company
+        return JobPosting.objects.filter(job_role__department__company=company)
