5ea9f3fc9313b35e45f2f58b104f30334ea10283
Shahriar-Ferdoush <1805101@ugrad.cse.buet.ac.bd>
2023-08-21 14:04:51 +0600

    Updated Navbar links

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,24 +1,30 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Stack , Button, IconButton } from "@mui/material";
+import { AppBar, Toolbar, Typography, Stack, Button } from "@mui/material";
 import { Link } from "react-router-dom"; // Import the Link component for navigation
 import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon";
 
 const Navbar = () => {
   return (
     <AppBar position="static">
-        <Toolbar>
-            {/* <IconButton> 
-                <CatchingPokemonIcon />
-            </IconButton> */}
-            <Typography sx={{flexGrow:1}}>
-              <h2> HRXcelerate </h2>
-            </Typography>
-            <Stack direction="row" spacing={2}>
-                <Button color='inherit'>Basic Info</Button>
-                <Button color='inherit'>Departments</Button>
-                <Button color='inherit'>Employee</Button>
-            </Stack>
-        </Toolbar>
+      <Toolbar>
+        {/* <IconButton> 
+            <CatchingPokemonIcon />
+        </IconButton> */}
+        <Typography sx={{ flexGrow: 1 }}>
+          <h2> HRXcelerate </h2>
+        </Typography>
+        <Stack direction="row" spacing={2}>
+          <Button color="inherit" component={Link} to="/basicinfo">
+            Basic Info
+          </Button>
+          <Button color="inherit" component={Link} to="/departments">
+            Departments
+          </Button>
+          <Button color="inherit" component={Link} to="/employees">
+            Employee
+          </Button>
+        </Stack>
+      </Toolbar>
     </AppBar>
   );
 };
