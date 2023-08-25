4866530c29601abe6ee1f63f4e94f93c2b25a57b
utchchhwas <usingha00@gmail.com>
2023-08-26 00:41:15 +0600

    Added Material UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,4 @@
-import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
-
-import SignUp from './pages/SignUp';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <>
-        <h1>Root Page</h1>,
-        <Outlet />
-      </>
-    ),
-    children: [
-      {
-        path: '/homepage',
-        element: <h1>Homepage</h1>,
-      },
-    ],
-  },
-  {
-    path: '/sign-up',
-    element: <SignUp />,
-  },
-]);
-
-export default function App() {
-  return <RouterProvider router={router} fallbackElement={<AppFallback />} />;
-}
-
-const AppFallback = () => {
-  return <h1>App is Loading...</h1>;
+const App = () => {
+  return <div>App</div>;
 };
+export default App;
--- frontend/src/pages/SignUp.jsx
+++
@@ -1,5 +0,0 @@
-const SignUp = () => {
-  return <h1>Sign-Up Page</h1>;
-};
-
-export default SignUp;
