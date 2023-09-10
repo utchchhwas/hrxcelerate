0b6f659c8f7cbc37eb4aae10077270fe59e41f1a
Shahriar-Ferdoush <1805101@ugrad.cse.buet.ac.bd>
2023-09-10 21:09:47 +0600

    Made EMployment Page

---
+++ frontend-sifat/src/Components/Employments.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import Navbar from "./Navbar";
+import EmploymentList from "./EmploymentList";
+
+function Employments() {
+  return (
+    <div>
+      <Navbar />
+      <EmploymentList />
+    </div>
+  );
+}
+
+export default Employments;
